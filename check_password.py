dc4f7c9ea6caf9b3dc9b538508e3fa873e85cca9
Rory Lee <rory.lee.work@gmail.com>
2026-03-09 19:16:29 +0000

    Update check_password.py

--- check_password.py
+++ check_password.py
@@ -1,19 +1,40 @@
 #------------------------------
 #Password Checker Application
 #------------------------------
+"""
 
+TODO: Add feedback, Add strength level, remove unnecessary brackets, show precise python confidence:(
+
+def is_length(password):
+        return len(password) >= 8
+
+)
+
+"""
 
 #------------------------------
-#Variables
+#Variable initialisation
 #------------------------------
-
-
+special_characters = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "+", "="]
+password_viability = 0
 #------------------------------
 #Subprograms
 #------------------------------
 def is_upper_and_lower(password):
-  if (any(char.isupper() for char in password)):
-    return True
+  if (any(char.isupper() for char in password) and any(char.islower() for char in password)):
+    return (True)
+
+def is_number(password):
+  if (any(char.isdigit() for char in password)):
+    return (True)
+
+def is_special_character(password):
+  if (any(char in special_characters for char in password)):
+    return (True)
+
+def is_length(password):
+  if (len(password) >= 8):
+    return (True)
 
 
 #------------------------------
@@ -22,3 +43,13 @@ def is_upper_and_lower(password):
 print("Welcome to the password authenticator program.")
 user_password = input("Please enter your password: ")
 
+if (is_upper_and_lower(user_password)):
+  password_viability += 1
+if (is_number(user_password)):
+  password_viability += 1
+if (is_special_character(user_password)):
+  password_viability += 1
+if (is_length(user_password)):
+  password_viability += 1
+
+print(f"your password score is: {password_viability}")
