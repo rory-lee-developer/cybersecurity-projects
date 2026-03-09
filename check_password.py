d506d898246fa8668c68b1f17a612cff6028f980
Rory Lee <rory.lee.work@gmail.com>
2026-03-09 18:56:49 +0000

    Update check_password.py

--- check_password.py
+++ check_password.py
@@ -2,44 +2,23 @@
 #Password Checker Application
 #------------------------------
 
-"""
-TODO: 8 character threshold
-"""
 
 #------------------------------
 #Variables
 #------------------------------
-user_password_score = 0
 
 
 #------------------------------
 #Subprograms
 #------------------------------
-def above_character_threshold(password):
-  pass
-def has_lowercase_and_uppercase(password):
-  pass
-def has_digits(password):
-  pass
-def has_special_characters(password):
-  pass
+def is_upper_and_lower(password):
+  if (any(char.isupper() for char in password)):
+    return True
 
 
 #------------------------------
 #Main Program
 #------------------------------
-print("Welcome to my password checker program!")
-user_password = input("Please enter the password that you would like to check: ")
+print("Welcome to the password authenticator program.")
+user_password = input("Please enter your password: ")
 
-if above_character_threshold(user_password) == True:
-  user_password_score += 1
-  threshold_check = True
-if has_lowercase_and_uppercase(user_password) == True:
-  user_password_score += 1
-  lowercase_uppercase_check = True
-if has_digits(user_password) == True:
-  user_password_score += 1
-  digits_check = True
-if has_special_characters(user_password) == True:
-  user_password_score += 1
-  special_characters_check = True
