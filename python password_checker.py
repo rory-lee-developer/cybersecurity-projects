f61cc19f0f9c376c50d7f61809d44824453f3695
Rory Lee <rory.lee.work@gmail.com>
2026-03-10 19:00:16 +0000

    Refactor password strength checks for clarity

--- python password_checker.py
+++ python password_checker.py
@@ -31,45 +31,50 @@ def determine_strength(password_viability):
         return "fair"
     elif password_viability == 4:
         return "strong - don't forget to change it regularly!"
-    else:
-        return "Sorry an error occcurred, please try again"
 #------------------------------
 #Main Program
 #------------------------------
 print("Welcome to the password authenticator program.")
 user_password = input("Please enter your password: ")
 
-if is_upper_and_lower(user_password):
+is_upper_and_lower_result = is_upper_and_lower(user_password)
+is_number_result = is_number(user_password)
+is_special_character_result = is_special_character(user_password)
+is_length_result = is_length(user_password)
+
+
+if is_upper_and_lower_result:
     password_viability += 1
-if is_number(user_password):
+if is_number_result:
     password_viability += 1
-if is_special_character(user_password):
+if is_special_character_result:
     password_viability += 1
-if is_length(user_password):
+if is_length_result:
     password_viability += 1
     
-if not all([is_upper_and_lower(user_password), is_number(user_password), 
-            is_special_character(user_password), is_length(user_password)]):
+determine_strength_result = determine_strength(password_viability)
+
+if not all([is_upper_and_lower_result, is_number_result, is_special_character_result, is_length_result]):
     print("\n-----------------------------------------------------------------")
 
-if not is_upper_and_lower(user_password):
+if not is_upper_and_lower_result:
     print("\nYour password must include both upper and lowercase characters.")
-if not is_number(user_password):
+if not is_number_result:
     print("\nYour password must include at least one digit.")
-if not is_special_character(user_password):
+if not is_special_character_result:
     print("\nYour password must include at least one special character.")
-if not is_length(user_password):
+if not is_length_result:
     print("\nYour password must be at least 8 characters (12+ recommended).")
     
     
 print(f"\nYour password score is: {password_viability}/4")
-print(f"Your password strength score is: {determine_strength(password_viability)}")
-
+print(f"Your password strength score is: {determine_strength_result}")
 
 """
 TODO:
 
 Weaknesses and vulnerabilities currently include (but are not limited to): hardcoded special character set, doesn't account for dictionary words, passworded is not hidden during input (Shoulder surfing risk), doesn't account for common passwords (e.g. Password123!)
-possible improvements: "getpass" library to avoid shoulder surfing, check against a common password list, "string" library for special characters, perhaps use password entropy
-also: avoid multiple function calls by saving their outputs as strings, build password entropy calculator
+possible improvements: "getpass" library to avoid shoulder surfing, check against a common password list, "string" library for special characters
+also: build password entropy calculator
+
 """
