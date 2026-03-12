cc5c6781a17adf51794416eab49d6e70390aa6ca
Rory Lee <rory.lee.work@gmail.com>
2026-03-12 19:16:03 +0000

    Refactor password checker logic and improve checks
    Refactor password checking logic to improve clarity and functionality. Replace hardcoded special characters with string.punctuation and streamline password viability checks.

--- python password_checker.py
+++ python password_checker.py
@@ -13,8 +13,7 @@ import string
 #Variable initialisation
 #------------------------------
 
-special_characters = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", ":", ";"] # I am aware that this is hardcoded and incomplete
-password_viability = 0
+SPECIAL_CHARACTERS = string.punctuation
 
 #------------------------------
 #Subprograms
@@ -27,7 +26,7 @@ def is_number(password):
     return any(char.isdigit() for char in password)
 
 def is_special_character(password):
-    return any(char in special_characters for char in password)
+    return any(char in SPECIAL_CHARACTERS for char in password)
 
 def is_length(password):
     return len(password) >= 8
@@ -37,53 +36,53 @@ def determine_strength(password_viability):
         return "poor"
     elif password_viability <= 3:
         return "fair"
-    elif password_viability == 4:
+    else:
         return "strong - don't forget to change it regularly!"
 
 #------------------------------
 #Main Program
 #------------------------------
 print("Welcome to the password authenticator program.")
-user_password = getpass.getpass("Enter your password: ") # WARNING: must have appropriate output terminal to censor password
 
-is_upper_and_lower_result = is_upper_and_lower(user_password)
-is_number_result = is_number(user_password)
-is_special_character_result = is_special_character(user_password)
-is_length_result = is_length(user_password)
+password_viability = 0
 
-
-if is_upper_and_lower_result:
-    password_viability += 1
-if is_number_result:
-    password_viability += 1
-if is_special_character_result:
-    password_viability += 1
-if is_length_result:
-    password_viability += 1
+while password_viability < 4:
+    password_viability = 0
+    user_password = getpass.getpass("Enter your password: ") # WARNING: must have appropriate output terminal to censor password
     
-determine_strength_result = determine_strength(password_viability)
-
-if not all([is_upper_and_lower_result, is_number_result, is_special_character_result, is_length_result]):
-    print("\n-----------------------------------------------------------------")
-
-if not is_upper_and_lower_result:
-    print("\nYour password must include both upper and lowercase characters.")
-if not is_number_result:
-    print("\nYour password must include at least one digit.")
-if not is_special_character_result:
-    print("\nYour password must include at least one special character.")
-if not is_length_result:
-    print("\nYour password must be at least 8 characters (12+ recommended).")
+    is_upper_and_lower_result = is_upper_and_lower(user_password)
+    is_number_result = is_number(user_password)
+    is_special_character_result = is_special_character(user_password)
+    is_length_result = is_length(user_password)
     
     
-print(f"\nYour password score is: {password_viability}/4")
-print(f"Your password strength score is: {determine_strength_result}")
-
+    password_viability = (
+        is_upper_and_lower_result + is_number_result + is_special_character_result + is_length_result
+    )
+        
+    determine_strength_result = determine_strength(password_viability)
+    
+    if not all([is_upper_and_lower_result, is_number_result, is_special_character_result, is_length_result]):
+        print("\n-----------------------------------------------------------------")
+    
+    if not is_upper_and_lower_result:
+        print("\nYour password must include both upper and lowercase characters.")
+    if not is_number_result:
+        print("\nYour password must include at least one digit.")
+    if not is_special_character_result:
+        print("\nYour password must include at least one special character.")
+    if not is_length_result:
+        print("\nYour password must be at least 8 characters (12+ recommended).")
+        
+        
+    print(f"\nYour password score is: {password_viability}/4")
+    print(f"Your password strength score is: {determine_strength_result}")
 """
 TODO:
 
-Weaknesses and vulnerabilities currently include (but are not limited to): hardcoded special character set, doesn't account for dictionary words, passworded is not hidden during input (Shoulder surfing risk), doesn't account for common passwords (e.g. Password123!)
-possible improvements: check against a common password list, "string" library for special characters
+Weaknesses and vulnerabilities currently include (but are not limited to):doesn't account for dictionary words, , doesn't account for common passwords (e.g. Password123!)
+possible improvements: check against a common password list
 also: build password entropy calculator, add a random password generator for the user for strong passwords
+Code quality: Better separation of logic/function structure
 
 """
